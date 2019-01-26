aefcbaca7bd13c69942816b11c8283e7efb50003
aseksenali <asetseksenali@gmail.com>
2019-01-27 00:00:38 +0600

    Minecraft 1.0.0

---
+++ Lab1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        string name;
+        int id;
+        int year;
+        public Student(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine(name + " " + id);
+            year++;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
