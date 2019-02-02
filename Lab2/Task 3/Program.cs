4e58d72ccae5e470653947f0ce31e2efa89a63fb
aseksenali <asetseksenali@gmail.com>
2019-02-02 17:35:26 +0600

    Minecraft 1.3.5

---
+++ Lab2/Task 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            DirectoryInfo file = new DirectoryInfo(path);
+            PrintInfo(file, 0);
+        }
+
+        private static void PrintInfo(FileSystemInfo file, int k)
+        {
+            if (file.GetType() == typeof(DirectoryInfo))
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            } else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+
+            Console.WriteLine(new string(' ', k) + file.Name);
+            if (file.GetType() == typeof(DirectoryInfo))
+            {
+                var y = file as DirectoryInfo;
+                foreach (var x in y.EnumerateFileSystemInfos())
+                {
+                    PrintInfo(x, k + 3);
+                }
+            }
+        }
+    }
+}
---
+++ Lab2/Task 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            string path1 = Console.ReadLine();
+
+            FileStream fs = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
+            FileStream fs2 = new FileStream(path1, FileMode.Create, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            StreamReader sr = new StreamReader(fs);
+            StreamWriter sw2 = new StreamWriter(fs2);
+
+            sw.Write("Original");
+
+            string text = sr.ReadToEnd();
+            sw2.Write(text);
+            File.Delete(path);
+
+            sr.Close();
+            sw2.Close();
+            sw.Close();
+            fs.Close();
+            fs2.Close();
+        }
+    }
+}
