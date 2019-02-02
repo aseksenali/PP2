5bc8236d82bf33956f251746e23c888c16d5dacb
aseksenali <asetseksenali@gmail.com>
2019-02-02 15:27:39 +0600

    Minecraft 1.2.1

---
+++ Lab1/Task 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+
+            int[] b = Repeat(a);
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write(b[i] + " ");
+            }
+        }
+
+        public static int[] Repeat(int[] a)
+        {
+            int[] b = new int[a.Length * 2];
+            for (int i = 0; i < a.Length; i++)
+            {
+                b[2 * i] = a[i];
+                b[2 * i + 1] = a[i];
+            }
+
+            return b;
+        }
+    }
+}
